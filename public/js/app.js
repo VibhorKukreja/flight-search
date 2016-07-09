a314d7efcc0a151711d79f1a68e93ef49b7b5910
Arun Kumar <arun.kumar@tothenew.com>
2016-07-09 17:04:37 +0530

    integrated airport list api

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,30 @@
 /**
  * Created by arun on 9/7/16.
  */
-$(function() {
-
-    $( "#from" ).autocomplete({
+function _fetchAirportsList(selector){
+    selector = $('#'+selector);
+    selector.autocomplete({
         minLength: 2,
-        source: 'http://node.locomote.com/code-task/airports',
+        delay : 500,
+        source: '/flights/airports',
         focus: function( event, ui ) {
-            $( "#from" ).val( ui.item.label );
+            selector.val( ui.item.airportName+' ('+ ui.item.airportCode +')' );
             return false;
         },
         select: function( event, ui ) {
-            $( "#from" ).val( ui.item.label );
-
+            selector.val( ui.item.airportName+' ('+ ui.item.airportCode +')' );
+            selector.attr('data-airport-code', ui.item.airportCode);
             return false;
         }
     })
-        .autocomplete( "instance" )._renderItem = function( ul, item ) {
-        console.log(item);
-        /*return $( "<li>" )
-            .append( "<a>" + item.label + "<br>" + item.desc + "</a>" )
-            .appendTo( ul );*/
+    .autocomplete( "instance" )._renderItem = function( ul, item ) {
+        return $( "<li>" )
+            .append( "<a>" + item.airportName + "<br>" + item.cityName +", "+ item.countryName + "</a>" )
+            .appendTo( ul );
     };
+}
+
+$(function(){
+    _fetchAirportsList('fromLocation');
+    _fetchAirportsList('toLocation');
 });
